640b6418af0a4e420b741d9d52a6642870d49911
hahw0627 <hahw0627@naver.com>
2024-07-04 13:04:42 +0900

    feat : DamageTxt
    데미지 표시 추가

---
+++ Assets/02.Scripts/DamageText.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Runtime.InteropServices;
+
+public class DamageText : MonoBehaviour
+{
+    public float moveSpeed; // 텍스트 이동속도
+    public float alphaSpeed; // 투명도 변환속도
+    public float destroyTime;
+    TextMeshPro text;
+    Color alpha;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+        text.text = damage.ToString();
+        alpha = text.color;
+        Invoke("DestroyObject", destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
+        alpha.a = Mathf.Lerp(alpha.a, 0, Time.deltaTime * alphaSpeed);
+        text.color = alpha;
+    }
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
