8a110f089a1e6595dc7b7e0d2fe49aae33809126
박의겸 <qkrdml9@naver.com>
2024-08-14 15:18:59 +0900

    update_스테이지 매니저 수정
    WaitTime 메서드 추가

--- Assets/02. Scripts/Stage/StageManager.cs
+++ Assets/02. Scripts/Stage/StageManager.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class StageManager : SingletonDestroyable<StageManager>
 {
     public event Action<int, int> onStageChanged;
-    
+
     [SerializeField]
     private TextMeshProUGUI _stageTmp;
 
@@ -23,14 +24,24 @@ public class StageManager : SingletonDestroyable<StageManager>
     {
         UpdateStageDisplay(StageDataSO.Stage, StageDataSO.StagePage);
         onStageChanged += UpdateStageDisplay;
-        StartCoroutine(_monsterSpawner.CheckMonsters());
+        
+        if (StageDataSO.Stage == 1 && StageDataSO.StagePage == 0)
+        {
+            StartCoroutine(WaitTime(4.0f));
+            StartCoroutine(_monsterSpawner.CheckMonsters());
+        }
     }
-    
+
+    public IEnumerator WaitTime(float num)
+    {
+        yield return new WaitForSeconds(num);
+    }
+
     public void ChangeStage(int newStage, int newStagePage)
     {
         onStageChanged?.Invoke(newStage, newStagePage);
     }
-    
+
     private void UpdateStageDisplay(int newStage, int newStagePage)
     {
         _stageTmp.text = $"스테이지 {newStage}-{newStagePage}";
