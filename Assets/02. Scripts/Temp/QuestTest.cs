51bfae856b256b4624f478abed95bfcb779b8373
nakwony <109271674+nakwony@users.noreply.github.com>
2024-08-22 12:28:28 +0900

    feat: 퀘스트 성공 알림 전단지 추가

--- Assets/02. Scripts/Temp/QuestTest.cs
+++ Assets/02. Scripts/Temp/QuestTest.cs
@@ -1,5 +1,5 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Quest.Core;
 using Quest.Core.Task.Target.Base;
 using TMPro;
@@ -20,7 +20,17 @@ public class QuestTest : SingletonDestroyable<QuestTest>
     private GameObject _questImage;
 
     [SerializeField]
+    private GameObject _badgeImage; // 도장 이미지 추가
+
+    [SerializeField]
     private TextMeshProUGUI _questDescription;
+
+    [SerializeField]
+    private GoldAcquireEffect _goldAcquireEffect;
+
+    [SerializeField]
+    private Transform _startPositionTransformOfEffect;
+
 
     private int _currentQuestIndex; // 현재 진행 중인 퀘스트 인덱스
 
@@ -39,36 +49,50 @@ public class QuestTest : SingletonDestroyable<QuestTest>
             print($"<color=orange>Quest:{quest.CodeName} Completed</color>");
             print($"<color=orange>Completed Quests Count:{questSystem.CompletedQuests.Count}</color>");
 
-            // 현재 퀘스트가 완료되면 다음 퀘스트를 등록
-            _currentQuestIndex++;
-            RegisterNextQuest(questSystem);
+            // 현재 퀘스트가 완료되면 보상 추가
             DataManager.Instance.AddGem(_questList[_currentQuestIndex].Rewards[0].Quantity);
-            Debug.Log("<color=red>수배 전단지 등장!</color>");
-            _questImage.SetActive(true);
-            _questDescription.text = _questList[_currentQuestIndex].Description;
+            StartCoroutine(ShowCompletedQuest(questSystem));
         };
 
         // 첫 번째 퀘스트 등록
-        RegisterNextQuest(questSystem);
-        _questImage.SetActive(true);
-        _questDescription.text = _questList[_currentQuestIndex].Description;
+        RegisterQuest(questSystem, _currentQuestIndex);
     }
 
-    private void RegisterNextQuest(QuestSystem questSystem)
+    private void RegisterQuest(QuestSystem questSystem, int questIndex)
     {
-        if (_currentQuestIndex < _questList.Count)
+        if (questIndex >= _questList.Count) return;
+        var quest = questSystem.Register(_questList[questIndex]);
+        quest.onTaskSuccessChanged += (quest1, task, currentSuccess, _) =>
         {
-            var newQuest = questSystem.Register(_questList[_currentQuestIndex]);
-            newQuest.onTaskSuccessChanged += (quest1, task, currentSuccess, _) =>
-            {
-                print(
-                    $"<color=orange>Quest:{quest1.CodeName}, Task:{task.CodeName}, CurrentSuccess:{currentSuccess}</color>");
-            };
-        }
-        else
-        {
-            print("<color=green>All quests have been registered.</color>");
-        }
+            print($"<color=orange>Quest:{quest1.CodeName}, Task:{task.CodeName}, CurrentSuccess:{currentSuccess}</color>");
+        };
+
+        // 퀘스트 이미지 및 설명 설정
+        _questImage.SetActive(true);
+        _questDescription.text = _questList[questIndex].Description;
+    }
+
+    private IEnumerator ShowCompletedQuest(QuestSystem questSystem)
+    {
+        // 직전 퀘스트의 이미지와 설명 활성화
+        _questImage.SetActive(true);
+        _questDescription.text = _questList[_currentQuestIndex].Description; // 완료된 퀘스트의 설명
+
+        // 도장 이미지 활성화
+        _badgeImage.SetActive(true);
+        
+        yield return new WaitForSecondsRealtime(3f); // 5초 대기
+        
+        // 도장 이미지 비활성화
+        _questImage.SetActive(false);
+        _badgeImage.SetActive(false);
+        _goldAcquireEffect.PlayGoldAcquireEffect(_startPositionTransformOfEffect.position,
+            _questList[_currentQuestIndex].Rewards[0].Quantity); // ���� ����Ʈ ����
+        
+        yield return new WaitForSecondsRealtime(3f); // 5초 대기
+
+        _currentQuestIndex++; // 다음 퀘스트 인덱스 증가
+        RegisterQuest(questSystem, _currentQuestIndex); // 다음 퀘스트 등록
     }
 
     public void CountOneQuestSuccess()
